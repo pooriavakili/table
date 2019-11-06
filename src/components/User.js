c92091b6bc430ee2d9d9e1b00fbfe5d57405b661
pooriavakili <pooriavakili09@gmail.com>
2019-11-06 12:48:47 +0330

    first commit

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react'
+
+const User=()=>(
+    <Fragment>
+        <div>
+            <h1>
+                user
+            </h1>
+        </div>
+    </Fragment>
+)
+export default User
